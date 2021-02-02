4a12eb015c8ee2ad58d874d916be575ab211358c
Amelia Goodman <amelia.goodman1@gmail.com>
2021-02-02 12:45:03 -0800

    log

--- dogbot.py
+++ dogbot.py
@@ -58,6 +58,8 @@ def hello_dog():
         ]
     }
     print(data.get('channel_id'))
+    print("DATA----------")
+    print(data)
     slack_request = requests.post(SLACK_URL, json=slack_payload)
     return 'Hello, Dog!'
 
