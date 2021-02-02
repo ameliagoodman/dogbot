e0c9ad0a4f84025911f6a37e2d89752813ddfc94
Amelia Goodman <amelia.goodman1@gmail.com>
2021-02-02 10:34:03 -0800

    dogbot

---
+++ dogbot.py
@@ -0,0 +1,59 @@
+import requests
+import random
+import os
+from flask import Flask, request
+app = Flask(__name__)
+
+# @app.rout('/new_dog', methods=[])
+
+@app.route('/fetch', methods=['GET', 'POST'])
+def hello_dog():
+    API_KEY =  os.getenv("GIPHY")
+    data = request.headers
+    giphy_payload = {'api_key': API_KEY, 'q': 'dog', 'limit': 1, 'offset': random.randint(0, 1000), 'rating': 'g'}
+    giphy_request = requests.get('http://api.giphy.com/v1/gifs/search', params=giphy_payload)
+    giphy_response = giphy_request.json()
+    gif_URL = giphy_response.get('data', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9')[0].get('images', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9').get('downsized', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9').get('url', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9')
+
+    slack_payload = {
+        "response_type": "in_channel",
+        "blocks": [
+            {
+                "type": "image",
+                "image_url": gif_URL,
+                "alt_text": "dog",
+                "title": {
+                    "type": "plain_text",
+                    "text": "happy monday!"
+                }
+            }, 
+            {
+                "type": "actions",
+                "block_id": "newdog?",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "good dog"
+                        },
+                        "style": "primary",
+                        "action_id": "send_dog",
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "grrr"
+                        },
+                        "style": "danger",
+                        "action_id": "new_dog",
+                    }
+                ]
+                
+            }
+        ]
+    }
+    slack_request = requests.post('https://hooks.slack.com/services/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', json=slack_payload)
+    return 'Hello, Dog!'
+
