e1d1e840caa1383215080504a5ba9f6ebec0ba07
Amelia Goodman <amelia.goodman1@gmail.com>
2021-02-02 13:27:38 -0800

    clean

--- dogbot.py
+++ dogbot.py
@@ -4,21 +4,26 @@ import os
 from flask import Flask, request
 app = Flask(__name__)
 
+def get_gif():
+    API_KEY =  os.getenv("GIPHY")
+    
+
+    giphy_payload = {'api_key': API_KEY, 'q': 'dog', 'limit': 1, 'offset': random.randint(0, 1000), 'rating': 'g'}
+    giphy_request = requests.get('http://api.giphy.com/v1/gifs/search', params=giphy_payload)
+    giphy_response = giphy_request.json()
+    gif_URL = giphy_response.get('data', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9')[0].get('images', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9').get('downsized', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9').get('url', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9')
+    return gif_URL
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return "Hello dog"
 
 @app.route('/fetch', methods=['GET', 'POST'])
 def hello_dog():
-    API_KEY =  os.getenv("GIPHY")
     SLACK_URL =  os.getenv("SLACK_URL")
     data = request.form
     
-    giphy_payload = {'api_key': API_KEY, 'q': 'dog', 'limit': 1, 'offset': random.randint(0, 1000), 'rating': 'g'}
-    giphy_request = requests.get('http://api.giphy.com/v1/gifs/search', params=giphy_payload)
-    giphy_response = giphy_request.json()
-    gif_URL = giphy_response.get('data', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9')[0].get('images', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9').get('downsized', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9').get('url', 'https://giphy.com/gifs/4Zo41lhzKt6iZ8xff9')
-
+    gif_URL = get_gif()
     slack_payload = {
         "response_type": "in_channel",
         "blocks": [
@@ -36,5 +41,5 @@ def hello_dog():
 
     
     slack_request = requests.post(SLACK_URL + data['channel_id'], json=slack_payload)
-    return
+    return "arf"
 
