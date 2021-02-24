62a08993489a548a1dc13dfdc435e0fde90b02a3
Amelia Goodman <amelia.goodman1@gmail.com>
2021-02-23 16:38:37 -0800

    remove link

--- mondog.py
+++ mondog.py
@@ -22,7 +22,7 @@ def main():
             ]
         }
         
-        r = requests.post("https://hooks.slack.com/services/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", json=slack_payload)
+        r = requests.post(DOGS_CHANNEL, json=slack_payload)
         print(r.status_code)
         return
 
